41915d65dc61b0470c1c9fc8c4e5b8e8d62d17eb
Connie Leung <cleung334@gmail.com>
2025-08-17 15:44:21 +0800

    feat: create an AlertList component

---
+++ src/app/alert-list/alert-list.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-list',
+  imports: [],
+  templateUrl: './alert-list.component.html',
+  styleUrl: './alert-list.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AlertListComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AlertListComponent } from './alert-list/alert-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [AlertListComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  protected title = 'angular-Alert-component';
-}
+export class AppComponent {}
