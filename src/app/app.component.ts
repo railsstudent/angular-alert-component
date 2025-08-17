450682dadf8cbee4e5b56bba4ebf310fd8f572c4
Connie Leung <cleung334@gmail.com>
2025-08-17 20:05:16 +0800

    fix: styling of the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,21 @@ import { AlertType } from './alert.type';
   selector: 'app-root',
   imports: [AlertListComponent],
   template: `
-<main>
-  <app-alert-list [alerts]="alerts()" />
-</main>
+<div id="app">
+  <main>
+    <app-alert-list [alerts]="alerts()" />
+  </main>
+</div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: `
+    #app {
+      max-width: 1280px;
+      margin: 0 auto;
+      padding: 2rem;
+      font-weight: 400;
+    }
+  `
 })
 export class AppComponent {
   alerts = signal<{ type: AlertType, message: string }[]>([
