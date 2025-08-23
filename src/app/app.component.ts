b237402bac6d0d1eaaf24d5b580cd14d181bb9ef
Connie Leung <cleung334@gmail.com>
2025-08-24 07:23:34 +0800

    refactor: move alerts to const

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { AlertListComponent } from './alert-list/alert-list.component';
 import { AlertType } from './alert.type';
+import { ALERTS } from './app.const';
+
+
 
 @Component({
   selector: 'app-root',
@@ -23,21 +26,5 @@ import { AlertType } from './alert.type';
   `
 })
 export class AppComponent {
-  alerts = signal<{ type: AlertType, message: string }[]>([
-    { 
-      type: 'info',
-      message: 'New software update available.'
-    }, 
-    { 
-      type: 'success',
-      message: 'Your purchase has been confirmed!'
-    }, 
-    { 
-      type: 'warning',
-      message: 'Warning: Invalid email address!'
-    }, 
-    { 
-      type: 'error',
-      message: 'Error! Task failed successfully.'
-    }])
+  alerts = signal<{ type: AlertType, message: string }[]>(ALERTS)
 }
